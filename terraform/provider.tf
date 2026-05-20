2be3285039ba1bd82da827b6ca727582e4337413
yuhyunsang06 <delipure@naver.com>
2026-05-20 15:27:12 +0900

    수정함

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,8 +8,8 @@ terraform {
     }
   }
    backend "s3" {
-    bucket = "tfstate-bucket-021534ba"
-    key = "test01/terraform.tfstate" #/test01/하위에 만들어 지도록
+    bucket = "tfstate-bucket-7a8a8149"
+    key = "test02/terraform.tfstate" #/test01/하위에 만들어 지도록
     region = "ap-northeast-2"
     dynamodb_table = "terraform-lock-test02" # 미리 준비된 dynamodb 테이블의 이름을 명시하면 lock 상태가 자동으로 관리된다.
     encrypt = true #tfstate에는 민감한 정보가 있을수 있기 때문에 암호화
